1cd3c4826ae3bbf5645a95c235e4a3dd9314a456
yonnatanbustos <yebustosr@uqvirtual.edu.co>
2019-10-28 23:22:11 -0500

    Creando mi primera prueba unitaria

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
@@ -1,9 +1,45 @@
 package com.hbt.semillero.servicios;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class AppTest {
 
 	public void shouldAnswerWithTrue() {
 
 	}
+	@Test
+	public void primeraPU() {
+		Long resultadoEsperado = 150L;
+		Long sumando1 = 100L;
+		Long sumando2 = 50L;
+		Long resultado = sumando1 + sumando2;
+		Assert.assertEquals(resultado, resultadoEsperado);
+		resultadoEsperado = 200L;
+		Assert.assertNotEquals(resultado, resultadoEsperado);
+	}
+	
+	private String invertirCadena(String cadena) {
+		String cadenaInvertida = "";
+		for (int x = cadena.length() - 1; x >= 0; x--) {
+            cadenaInvertida = cadenaInvertida + cadena.charAt(x);
+		}
+		return cadenaInvertida;
+
+	}
+	/**
+	 * Prueba unitaria para una cadena invertida
+	 */
+	@Test
+	public void segundaPU() {
+		String cadena = "hola mundo";
+		String cadenaInvertida = "odnum aloh";
+		String cadenaEsperada = invertirCadena(cadena);
+		Assert.assertEquals(cadenaInvertida, cadenaEsperada);
+		Assert.assertNotEquals(cadena, cadenaEsperada);
+
+}
+
+	
+	
 }
